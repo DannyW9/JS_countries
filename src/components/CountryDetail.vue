42f9a9e103311a8671b2375a01f0dafca9d4a4db
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-01 16:07:44 +0100

    Adds MVP finished

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div v-if="country">
+    <h3>{{country.name}}</h3>
+    <p>Population: {{country.population}}</p>
+    <p>Capital: {{country.capital}}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+
+  <option @click="handleClick">{{country.name}}</option>
+
+</template>
+
+<script>
+import { eventBus } from '../main.js'
+export default {
+  name: 'list-component',
+  props: ['country'],
+  methods: {
+    handleClick: function(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
